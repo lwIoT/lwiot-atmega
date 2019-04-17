d9da4c6c2c5bc2467547771c70d5e0e7735926e3
Michel Megens <dev@bietje.net>
2019-04-17 13:17:04 +0200

    external: add references to external dependencies
    Add references to external libraries.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ standalone/lwiot_arch.h
@@ -0,0 +1,31 @@
+/*
+ * No RTOS architecture header.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#define CONFIG_CONFIG_STANDALONE 1
+
+#ifdef __cplusplus
+extern "C" void no_os_init(void);
+#else
+extern void no_os_init(void);
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct mutex {
+	uint8_t lock;
+#define HAVE_MUTEX
+} lwiot_mutex_t;
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
